f41d8e673f7b8fb20ee1602531ab5a38ef734863
Josh <chang101@aol.com>
2015-02-24 16:06:33 -0500

    Updated Charity Table with Index!
    Use this sql code and ignore the Charitydb sql.  This code has a column
    for the index

--- Charitydb.sql
+++ Charitydb.sql
@@ -3,17 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: gsarastestdb.gsaras.com
--- Generation Time: Feb 20, 2015 at 11:51 AM
+-- Generation Time: Feb 24, 2015 at 12:59 PM
 -- Server version: 5.1.56
 -- PHP Version: 5.4.37
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `gsarastestdb`
@@ -26,20 +20,22 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `Charities` (
+  `charityNum` mediumint(6) NOT NULL AUTO_INCREMENT,
   `CharityName` text NOT NULL,
   `PrivateSupport` float NOT NULL,
   `TotalRevenue` float NOT NULL,
   `FundraisingEfficiency` int(11) NOT NULL,
   `CharitableCommitment` int(11) NOT NULL,
-  `DonorDependency` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `DonorDependency` int(11) NOT NULL,
+  PRIMARY KEY (`charityNum`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `Charities`
 --
 
-INSERT INTO `Charities` (`CharityName`, `PrivateSupport`, `TotalRevenue`, `FundraisingEfficiency`, `CharitableCommitment`, `DonorDependency`) VALUES
-('Charity 1', 6.50042e+06, 1e+07, 90, 50, 100),
-('Charity 2', 4e+06, 5e+06, 10, 5, 0),
-('Charity 3', 250000, 1e+06, 75, 50, 90),
-('Charity 4', 750000, 2e+06, 90, 20, 86);
+INSERT INTO `Charities` (`charityNum`, `CharityName`, `PrivateSupport`, `TotalRevenue`, `FundraisingEfficiency`, `CharitableCommitment`, `DonorDependency`) VALUES
+(1, 'Charity 1', 6.50042e+06, 1e+07, 90, 50, 100),
+(2, 'Charity 2', 4e+06, 5e+06, 10, 5, 0),
+(3, 'Charity 3', 250000, 1e+06, 75, 50, 90),
+(4, 'Charity 4', 750000, 2e+06, 90, 20, 86);
